373fd158e7665b8203a6512136a7bcca06017fff
abhiyansh <abhiyansharya161@gmail.com>
2022-01-29 11:38:46 +0530

    add drag-to-draw and reset features

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+mouseDown = false;
+
 function createGridCell(cell_width){
     const grid_cell = document.createElement('div');
     
     grid_cell.classList.add('grid-cell');
+    
+    grid_cell.addEventListener('mouseenter', (event)=>mouseDown&&event.target.classList.add('visited'));
+    grid_cell.addEventListener('mousedown', (event)=>event.target.classList.add('visited'));
     
     grid_cell.style.width = cell_width+'px';
     grid_cell.style.height = cell_width+'px';
@@ -13,6 +18,9 @@ function createGridContainer(container_width){
     const grid_container = document.createElement('div');
     
     grid_container.classList.add('grid-container');
+
+    grid_container.addEventListener('mousedown', ()=>mouseDown = true);
+    grid_container.addEventListener('mouseup', ()=>mouseDown=false);
     
     grid_container.style.width = container_width+'px';
     grid_container.style.height = container_width+'px';
@@ -27,15 +35,22 @@ function createGrid(container_width, n){
     for(let i=0;i<(n*n);i++) grid_container.appendChild(createGridCell(cell_width));
 
     return grid_container;
+}
 
+function onReset(event){
+    gridCells = document.querySelectorAll('.grid-cell');
+    gridCells.forEach(element => element.classList.remove('visited'));
 }
 
 
 const container_width = 512;
-const n=16;
+const n=32;
 
 const body = document.querySelector('body');
 
 const grid_container = createGrid(container_width, n);
 
 body.appendChild(grid_container);
+
+resetButton = document.querySelector('button.reset');
+resetButton.addEventListener('click', onReset);
