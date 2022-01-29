359df0bacc1898a264a2d857ec6643e67e3f55c9
abhiyansh <abhiyansharya161@gmail.com>
2022-01-29 12:42:29 +0530

    restructuring, better organisation

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-mouseDown = false;
+let mouseDown = false;
 
 function createGridCell(cell_width){
     const grid_cell = document.createElement('div');
     
     grid_cell.classList.add('grid-cell');
     
-    grid_cell.addEventListener('mouseenter', (event)=>mouseDown&&event.target.classList.add('visited'));
-    grid_cell.addEventListener('mousedown', (event)=>event.target.classList.add('visited'));
+    grid_cell.addEventListener('mouseenter', (event)=>mouseDown&&onDrag(event));
+    grid_cell.addEventListener('mousedown', (event)=>onDrag(event));
     
     grid_cell.style.width = cell_width+'px';
     grid_cell.style.height = cell_width+'px';
@@ -14,43 +14,30 @@ function createGridCell(cell_width){
     return grid_cell
 }
 
-function createGridContainer(container_width){
-    const grid_container = document.createElement('div');
-    
-    grid_container.classList.add('grid-container');
-
-    grid_container.addEventListener('mousedown', ()=>mouseDown = true);
-    grid_container.addEventListener('mouseup', ()=>mouseDown=false);
-    
-    grid_container.style.width = container_width+'px';
-    grid_container.style.height = container_width+'px';
-
-    return grid_container;
+function createGrid(grid_container, n){
+    const cell_width = grid_container.clientHeight/n;
+    for(let i=0;i<(n*n);i++) grid_container.appendChild(createGridCell(cell_width));
 }
 
-function createGrid(container_width, n){
-    const cell_width = container_width/n;
-    const grid_container = createGridContainer(container_width);
-    
-    for(let i=0;i<(n*n);i++) grid_container.appendChild(createGridCell(cell_width));
-
-    return grid_container;
+function onDrag(event){
+    event.target.style['background-color']=penColor;
 }
 
 function onReset(event){
     gridCells = document.querySelectorAll('.grid-cell');
-    gridCells.forEach(element => element.classList.remove('visited'));
+    gridCells.forEach(element => element.style['background-color']='white');
 }
 
 
-const container_width = 512;
 const n=32;
+let penColor = 'black';
 
-const body = document.querySelector('body');
+const grid_container = document.querySelector('.grid-container');
 
-const grid_container = createGrid(container_width, n);
+grid_container.addEventListener('mousedown', ()=>mouseDown = true);
+grid_container.addEventListener('mouseup', ()=>mouseDown=false);
 
-body.appendChild(grid_container);
+createGrid(grid_container, n);
 
 resetButton = document.querySelector('button.reset');
 resetButton.addEventListener('click', onReset);
